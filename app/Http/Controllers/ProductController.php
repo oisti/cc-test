a4f04e8206193b98e19ab508b279e1fca6534485
Oláh István <istvan.olah@magicsolutions.ro>
2020-05-13 23:31:53 +0300

    Product, Category APIs

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Product;
+
+class ProductController extends Controller{
+    
+    public function search(Request $request){
+        $products = Product::query();
+        if ($request->has('category')){
+            $products->where('category_id', $request->category);
+        }
+        if ($request->has('searchTerm')){
+            $products->where('brand', 'like', '%'.$request->searchTerm.'%')
+                ->orWhere('name', 'like', '%'.$request->searchTerm.'%')
+                ->orWhere('size', 'like', '%'.$request->searchTerm.'%');
+        }
+        return $products->get();
+    }
+}
