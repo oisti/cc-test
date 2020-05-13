6d3bd4e5741b418b042e78997ced1fb5a044b06f
Oláh István <istvan.olah@magicsolutions.ro>
2020-05-13 22:34:44 +0300

    Product and Category models

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'category_id', 
+        'brand', 
+        'name', 
+        'promotion', 
+        'price', 
+        'discount_percent', 
+        'stock', 
+        'description', 
+        'image_url', 
+        'type', 
+        'size', 
+        'fuel_efficiency_class', 
+        'wet_grip_class', 
+        'noise_emission'
+    ];
 }
